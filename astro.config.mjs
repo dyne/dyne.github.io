f9c6add2a54843a8273dff464df6ff0c1c1706f3
Puria Nafisi Azizi <puria@dyne.org>
2022-10-12 23:23:00 +0200

    Add baselayout

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,12 @@ import partytown from '@astrojs/partytown';
 
 import sitemap from '@astrojs/sitemap';
 import { SITE } from './src/config.mjs';
+import image from '@astrojs/image';
+import path from 'path';
+
+import { fileURLToPath } from 'url';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 
 export default defineConfig({
 	site: SITE.origin,
@@ -14,5 +20,13 @@ export default defineConfig({
 		tailwind(),
 		partytown(),
 		sitemap(),
-	],
+        image()
+     ],
+    vite: {
+		resolve: {
+			alias: {
+				'~': path.resolve(__dirname, './src'),
+			},
+		},
+	},
 });
