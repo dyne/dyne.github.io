b331519115a9048d6164c07a48672ab3a0dfd407
Puria Nafisi Azizi <puria@dyne.org>
2022-11-01 06:57:07 +0100

    Setup the menu

--- src/config.mjs
+++ src/config.mjs
@@ -1,36 +1,9 @@
 export const SITE = {
 	name: 'Dyne.org',
-
 	origin: 'https://dyne.org',
 	basePathname: '/',
-
 	title: 'Dyne.org digital community and free software foundry',
 	description:
 		'🚀 A non-profit foundation and free software foundry with more than 10 years of expertise in developing tools and narratives for community empowerment.',
 	// '🚀 Dyne.org is a software foundry and research organization dedicated to share the benefits of freedom in the digital world. We share our ideas and inventions to promote horizontal access to technology, education and art.'
-};
-
-export const BLOG = {
-	disabled: false,
-	postsPerPage: 4,
-
-	blog: {
-		disabled: false,
-		pathname: 'blog', // blog main path, you can change this to "articles" (/articles)
-	},
-
-	post: {
-		disabled: false,
-		pathname: '', // empty for /some-post, value for /pathname/some-post
-	},
-
-	category: {
-		disabled: false,
-		pathname: 'category', // set empty to change from /category/some-category to /some-category
-	},
-
-	tag: {
-		disabled: false,
-		pathname: 'tag', // set empty to change from /tag/some-tag to /some-tag
-	},
 };
--- src/content/hero/index.md
+++ src/content/hero/index.md
@@ -2,6 +2,7 @@
 welcome : Welcome to our digital community and free software foundry! Dyne.org shares tools, practices and narratives that empower artists, creatives and citizens in the digital age.
 
 CTA: Learn More
+url: "#what-we-do"
 ---
 
 Free to share *code*.
