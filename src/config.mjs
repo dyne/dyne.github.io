c248d92fbfa3e2a62cfcc4cd635cca1843bac0b0
Puria Nafisi Azizi <puria@dyne.org>
2022-11-02 17:12:53 +0100

    Update default OG description

--- src/config.mjs
+++ src/config.mjs
@@ -3,7 +3,5 @@ export const SITE = {
 	origin: 'https://dyne.org',
 	basePathname: '/',
 	title: 'Dyne.org digital community and free software foundry',
-	description:
-		'🚀 A non-profit foundation and free software foundry with more than 10 years of expertise in developing tools and narratives for community empowerment.',
-	// '🚀 Dyne.org is a software foundry and research organization dedicated to share the benefits of freedom in the digital world. We share our ideas and inventions to promote horizontal access to technology, education and art.'
+	description: "🚀 Empowering artists, creatives and citizens for more than 20 years."
 };
