7340ca7ebb3a4f39d180b0608cbb373376f8c207
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2023-04-21 19:52:48 +0200

    feat( Timeline ): Reintroduced cards (#82)
    * feat: updated dates format
    
    * feat(Timeline): updated cards spacing
    
    * feat(Timeline): card instead of paragraph
    
    ---------
    
    Co-authored-by: Puria Nafisi Azizi <puria@dyne.org>

--- src/components/timeline/TimelineGroup.svelte
+++ src/components/timeline/TimelineGroup.svelte
@@ -9,9 +9,9 @@
 	<div class="pr-8">
 		<h2 class="text-primary text-7xl sticky left-0 w-fit">{group.year}</h2>
 	</div>
-	<div class="flex flex-nowrap border-t-[1px] border-t-primary space-x-10 pr-10">
+	<div class="flex flex-nowrap border-t-[1px] border-t-primary space-x-4 pr-8">
 		{#each group.items as item (item.title)}
-			<div class="shrink-0 w-[260px]">
+			<div class="shrink-0 w-[280px]">
 				<TimelineItem {item} />
 			</div>
 		{/each}
