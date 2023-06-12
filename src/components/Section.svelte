d8a9555843b2541d1501775d8cf42d6628572866
phoebus-84 <ennio@dyne.org>
2023-06-12 18:58:39 +0200

    style: in mobile homepage darker section with fade in and fade out

--- src/components/Section.svelte
+++ src/components/Section.svelte
@@ -3,18 +3,26 @@
 	export let description: string;
 	export let id = '';
 	export let contentPadding = true;
-	export let cls = "";
+	export let cls = '';
 </script>
 
-<section class="bg-saccent rounded-3xl text-white py-8 lg:py-12 space-y-8 lg:space-y-12 {cls}" {id}>
-	<div class="flex flex-col items-center lg:items-start space-y-8 lg:space-y-0">
-		<div class="font-semibold text-3x l lg:text-5xl whitespace-nowrap px-8 lg:px-12 text-background">{title}</div>
-		{#if description}
-			<div class="text-center lg:text-start text-lg px-8 lg:px-12 w-2/5 leading-tight pt-4 text-background">{description}</div>
-		{/if}
-	</div>
+<section {id}>
+	<div class="block lg:hidden h-20 bg-gradient-to-b from-background to-saccent" />
+	<div class="bg-saccent lg:rounded-3xl text-white lg:py-12 space-y-8 lg:space-y-12 {cls}" {id}>
+		<div class="flex flex-col items-center lg:items-start space-y-8 lg:space-y-0">
+			<div class="font-semibold text-3x text-5xl whitespace-nowrap px-8 pt-4 lg:pt-0 lg:px-12 text-background">
+				{title}
+			</div>
+			{#if description}
+				<div class="text-center lg:text-start text-lg lg:px-12 lg:w-2/5 leading-tight pt-4 text-background">
+					{description}
+				</div>
+			{/if}
+		</div>
 
-	<div class={`${contentPadding ? 'px-8 lg:px-12' : 'px-0'}`}>
-		<slot />
+		<div class={`${contentPadding ? 'px-8 lg:px-12' : 'px-0'}`}>
+			<slot />
+		</div>
 	</div>
+	<div class="block lg:hidden h-20 bg-gradient-to-t from-background to-saccent" />
 </section>
