6e1bd28dac195e3ee247a3d35ed5e47856bc2aaf
Jaromil <jaromil@dyne.org>
2025-08-24 10:47:21 +0200

    update astro to latest
    also add mdx and github markdown to be able to use inline html and
    components directly inside markdown inside .mdx files!
    
    works on my computer :^) needs review

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,22 +6,31 @@ import path from 'path';
 import { SITE } from './src/config.mjs';
 import { fileURLToPath } from 'url';
 import svelte from '@astrojs/svelte';
+import mdx from '@astrojs/mdx';
+import remarkGfm from 'remark-gfm';
+import rehypeRaw from 'rehype-raw';
+
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://astro.build/config
 export default defineConfig({
-	site: SITE.origin,
-	base: SITE.basePathname,
-	output: 'static',
-	markdown: {
-		drafts: true,
-	},
-	integrations: [tailwind(), partytown(), sitemap(), svelte()],
-	vite: {
-		resolve: {
-			alias: {
-				'~': path.resolve(__dirname, './src'),
-			},
-		},
-	},
+    site: SITE.origin,
+    base: SITE.basePathname,
+    output: 'static',
+    markdown: {
+        drafts: true,
+        remarkPlugins: [remarkGfm],
+        rehypePlugins: [rehypeRaw],
+        shikiConfig: {
+            theme: 'dracula',
+        },
+    },
+    integrations: [tailwind(), partytown(), sitemap(), svelte(), mdx()],
+    vite: {
+        resolve: {
+            alias: {
+                '~': path.resolve(__dirname, './src'),
+            },
+        },
+    },
 });
