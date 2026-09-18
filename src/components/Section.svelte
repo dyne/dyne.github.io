deb5d98f395207391ec5de269d417fee74f06fd1
mico_L <blurredmicol@gmail.com>
2026-09-18 13:14:24 +0200

    Small fixes to section headings and paddings

--- src/components/Section.svelte
+++ src/components/Section.svelte
@@ -11,13 +11,13 @@
 	Avoid using text-saccent or inverted buttons directly here,
 	as they rely on the light page background for contrast.
 -->
-	<div class="bg-saccent rounded-3xl md:py-12 text-white space-y-8 md:space-y-20 {cls}">
-		<div class="flex flex-col items-center md:items-start space-y-8 md:space-y-0">
-			<h2 class="not-prose !text-white font-display font-bold leading-heading text-5xl whitespace-nowrap pb-2 px-8 pt-4 md:pt-8 md:px-12">
+	<div class="bg-saccent rounded-3xl md:py-12 text-white space-y-8 pb-24 md:space-y-20 {cls}">
+		<div class="flex flex-col items-center md:items-start space-y-8 md:space-y-12">
+			<h2 class="not-prose !text-white text-center md:text-start font-display font-bold leading-heading text-4xl md:text-5xl whitespace-normal md:align-cente px-8 md:px-12 mt-12">
 				{title}
 			</h2>
 			{#if $$slots.description}
-				<div class="text-center md:text-start md:px-12 text-xl font-body leading-body md:w-3/5 pt-2">
+				<div class="not-prose text-center md:text-start px-8 md:px-12 text-xl md:text-2xl font-body leading-body whitespace-normal">
 					<slot name="description" />
 				</div>
 			{/if}
